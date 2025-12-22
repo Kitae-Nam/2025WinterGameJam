12e0abee98dbb10fcacc1edfae5394ade95a1464
Valay0429 <a49986661@gmail.com>
2025-12-22 15:30:57 +0900

    SoundUI

--- Assets/Branches/JJK/02.Scripts/OptionUI.cs
+++ Assets/Branches/JJK/02.Scripts/OptionUI.cs
@@ -6,13 +6,46 @@ using UnityEngine.UI;
 public class OptionUI : MonoBehaviour
 {
     [SerializeField] private GameObject optionUI;
+    
+    [Header("Sliders")]
     [SerializeField] private Slider bgmSlider;
     [SerializeField] private Slider sfxSlider;
+    
+    [Header("Icons")]
+    [SerializeField] private Image bgmIcon;
+    [SerializeField] private Image sfxIcon;
+    [SerializeField] private Sprite bgmOnSprite;
+    [SerializeField] private Sprite bgmMuteSprite;
+    [SerializeField] private Sprite sfxOnSprite;
+    [SerializeField] private Sprite sfxMuteSprite;
 
     private void Start()
     {
-        bgmSlider.value = SoundManager.Instance.GetBgmVolume();
-        sfxSlider.value = SoundManager.Instance.GetSfxVolume();
+        bgmSlider.SetValueWithoutNotify(SoundManager.Instance.GetBgmVolume());
+        sfxSlider.SetValueWithoutNotify(SoundManager.Instance.GetSfxVolume());
+        
+        UpdateIcon();
+        
+        bgmSlider.onValueChanged.AddListener(OnBgmSliderChanged);
+        sfxSlider.onValueChanged.AddListener(OnSfxSliderChanged);
+    }
+    
+    private void OnBgmSliderChanged(float value)
+    {
+        SoundManager.Instance.SetBgmVolume(value);
+        UpdateIcon();
+        
+        if (bgmSlider.value == 0f)
+            ToggleBgm();
+    }
+
+    private void OnSfxSliderChanged(float value)
+    {
+        SoundManager.Instance.SetSfxVolume(value);
+        UpdateIcon();
+        
+        if (sfxSlider.value == 0f)
+            ToggleSfx();
     }
 
     private void Update()
@@ -21,9 +54,51 @@ public class OptionUI : MonoBehaviour
             ToggleUI();
     }
 
+    public void ToggleBgm()
+    {
+        SoundManager.Instance.BgmMute();
+        UpdateIcon();
+    }
+
+    public void ToggleSfx()
+    {
+        
+        SoundManager.Instance.SfxMute();
+        UpdateIcon();
+    }
+
+    private void UpdateIcon()
+    {
+        bgmIcon.sprite = SoundManager.Instance.IsBgmMuted()
+            ? bgmMuteSprite
+            : bgmOnSprite;
+
+        sfxIcon.sprite = SoundManager.Instance.IsSfxMuted()
+            ? sfxMuteSprite
+            : sfxOnSprite;
+    }
+    
     public void ToggleUI()
     {
-        optionUI.SetActive(!optionUI.activeSelf);
-        Time.timeScale = optionUI.activeSelf ? 0 : 1;
+        bool isOpen = !optionUI.activeSelf;
+        optionUI.SetActive(isOpen);
+        Time.timeScale = isOpen ? 0 : 1;
+
+        if (isOpen)
+        {
+            bgmSlider.SetValueWithoutNotify(SoundManager.Instance.GetBgmVolume());
+            sfxSlider.SetValueWithoutNotify(SoundManager.Instance.GetSfxVolume());
+            UpdateIcon();
+        }
+    }
+
+    public void Again()
+    {
+        //다시 시작
+    }
+
+    public void Home()
+    {
+        //메뉴 화면으로
     }
 }

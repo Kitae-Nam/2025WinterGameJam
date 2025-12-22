88f97b09fe18579c41906c3ecae54427b1b05c0c
Valay0429 <a49986661@gmail.com>
2025-12-22 14:19:05 +0900

    MonoSingleton, SoundManager

--- Assets/Branches/JJK/02.Scripts/OptionUI.cs
+++ Assets/Branches/JJK/02.Scripts/OptionUI.cs
@@ -1,11 +1,20 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class OptionUI : MonoBehaviour
 {
     [SerializeField] private GameObject optionUI;
-    
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private void Start()
+    {
+        bgmSlider.value = SoundManager.Instance.GetBgmVolume();
+        sfxSlider.value = SoundManager.Instance.GetSfxVolume();
+    }
+
     private void Update()
     {
         if (Keyboard.current.escapeKey.wasPressedThisFrame)
